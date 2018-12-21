e1f004a3db353a217c16d10c1ccce551dc08467b
Mauricio Kruijer <mauricio.kruijer@gmail.com>
2018-12-21 13:57:19 +0100

    Added SlideShow component

--- slideshow/src/App.js
+++ slideshow/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
+import SlideShow from './components/layout/SlideShow';
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
         <div className="App">
           <h1>Photo Video Slideshow!</h1>
           <Navbar/>
+          <SlideShow/>
         </div>
       </BrowserRouter>
     );
---
+++ slideshow/src/components/layout/SlideShow.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class SlideShow extends Component {
+  render () {
+    return (
+      <div>
+        <div>
+          Super mooie SLIDE
+        </div>
+        <div>
+          Super vette Notifications
+        </div>
+      </div>
+    )
+  }
+}
+
+export default SlideShow
