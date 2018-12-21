6268ac6528e90b678bcf3a9c1febb69fdb373287
Mauricio Kruijer <mauricio.kruijer@gmail.com>
2018-12-21 16:06:32 +0100

    POC create fb record

--- slideshow/src/components/upload/Upload.js
+++ slideshow/src/components/upload/Upload.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
+import FirebaseContext from "../Firebase/Context";
 
 class Upload extends Component {
+  componentDidMount() {
+    const newPostKey = this.props.firebase.db.ref().child('slides').push().key;
+
+    this.props.firebase.db.ref(`slides/${newPostKey}`).set({
+      type: 'image',
+      url: 'jemoeder'
+    });
+  }
+
   render() {
     return (
       <div>
@@ -10,4 +20,6 @@ class Upload extends Component {
   }
 }
 
-export default Upload
+export default (props) => <FirebaseContext.Consumer>
+  { firebase => <Upload firebase={firebase} {...props}/> }
+</FirebaseContext.Consumer>
