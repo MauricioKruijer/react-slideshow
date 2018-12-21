b092dca03cfd5ecd66c04c21a1b46d8e5ebe3a01
Mauricio Kruijer <mauricio.kruijer@gmail.com>
2018-12-21 15:05:50 +0100

    Added firebase lib

--- slideshow/src/components/layout/SlideShow.js
+++ slideshow/src/components/layout/SlideShow.js
@@ -2,6 +2,24 @@ import React, { Component } from 'react';
 import Notifications from '../slideshow/Notification'
 
 class SlideShow extends Component {
+  state = {
+    currentSlide: {
+      url: '',
+      type: ''
+    },
+    queue: []
+  }
+
+  componentDidMount() {
+    // deal with firebase socket
+
+    // deal with queue
+  }
+
+  componentWillUnmount() {
+    // unsubscribe from firebase
+  }
+
   render () {
     return (
       <div>
