ed3f528ee17bb835ea78d191d476ee1b462d4ca2
Mauricio Kruijer <mauricio.kruijer@gmail.com>
2018-12-23 18:42:52 +0100

    Implement react-firebase-file-uploader
    Basic config and boilerplate code from
    https://github.com/fris-fruitig/react-firebase-file-uploader

--- slideshow/src/components/Firebase/Firebase.js
+++ slideshow/src/components/Firebase/Firebase.js
@@ -1,12 +1,13 @@
 import firebase from 'firebase/app';
 import 'firebase/database';
+import 'firebase/storage';
 
 const config = {
   apiKey: "AIzaSyAyuM9nLn1RHUv1j5PmWHmz7aJZkMbAvM4",
   authDomain: "react-slideshow-10adf.firebaseapp.com",
   databaseURL: "https://react-slideshow-10adf.firebaseio.com",
   projectId: "react-slideshow-10adf",
-  storageBucket: "",
+  storageBucket: "react-slideshow-10adf.appspot.com",
   messagingSenderId: "503357597586"
 };
 
@@ -14,6 +15,7 @@ class Firebase {
   constructor() {
       firebase.initializeApp(config);
       this.db = firebase.database()
+      this.storage = firebase.storage()
   }
 
   slides = () => {
--- slideshow/src/components/upload/Upload.js
+++ slideshow/src/components/upload/Upload.js
@@ -1,7 +1,37 @@
 import React, { Component } from 'react';
 import FirebaseContext from "../Firebase/Context";
+import FileUploader from 'react-firebase-file-uploader'
 
 class Upload extends Component {
+  state = {
+    username: "",
+    avatar: "",
+    isUploading: false,
+    progress: 0,
+    avatarURL: ""
+  };
+
+  handleChangeUsername = event => this.setState({ username: event.target.value });
+
+  handleUploadStart = () => this.setState({ isUploading: true, progress: 0 });
+
+  handleProgress = progress => this.setState({ progress });
+
+  handleUploadError = error => {
+    this.setState({ isUploading: false });
+    console.error(error);
+  };
+
+  handleUploadSuccess = filename => {
+    this.setState({ avatar: filename, progress: 100, isUploading: false });
+    this.props.firebase
+      .storage
+      .ref('files')
+      .child(filename)
+      .getDownloadURL()
+      .then(url => this.setState({ avatarURL: url }));
+  };
+
   componentDidMount() {
     this.props.firebase.db.ref(`slides`).push({
       type: 'image',
@@ -10,9 +40,34 @@ class Upload extends Component {
   }
 
   render() {
+    console.log('hoi')
+    console.log(this.props.firebase)
     return (
       <div>
         UPLOAD PAGEEE
+        <form>
+          <label>Username:</label>
+          <input
+            type="text"
+            value={this.state.username}
+            name="username"
+            onChange={this.handleChangeUsername}
+          />
+          <label>Avatar:</label>
+          {this.state.isUploading && <p>Progress: {this.state.progress}</p>}
+          {this.state.avatarURL && <img src={this.state.avatarURL} />}
+
+          <FileUploader
+            accept="video/*,image/*"
+            name="avatar"
+            randomizeFilename
+            storageRef={this.props.firebase.storage.ref('files')}
+            onUploadStart={this.handleUploadStart}
+            onUploadError={this.handleUploadError}
+            onUploadSuccess={this.handleUploadSuccess}
+            onProgress={this.handleProgress}
+          />
+        </form>
       </div>
     );
   }
