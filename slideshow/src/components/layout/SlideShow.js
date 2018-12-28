7f38397dcaab2cf37f69179bfc62450723945df5
Mauricio Kruijer <mauricio.kruijer@gmail.com>
2018-12-28 16:56:32 +0100

    prioritySlides queue

--- slideshow/src/components/layout/SlideShow.js
+++ slideshow/src/components/layout/SlideShow.js
@@ -23,6 +23,7 @@ const SlideComponents = {
 class SlideShow extends Component {
   canRunAfter = Date.now();
   slides = [];
+  prioritySlides = [];
   currentIndex = 0;
 
   state = {
@@ -64,7 +65,7 @@ class SlideShow extends Component {
       const nextIndex = (this.currentIndex + 1) % this.slides.length;
 
       this.setState(() => ({
-        currentItems: [this.slides[this.currentIndex]],
+        currentItems: [this.findCurrentItem()],
         nextItems: [this.slides[nextIndex]]
       }), () => {
         this.canRunAfter = Date.now() + duration;
@@ -73,18 +74,27 @@ class SlideShow extends Component {
     }, 1000);
   }
 
+  findCurrentItem() {
+    const currentSlide = this.slides[this.currentIndex];
+    if (this.prioritySlides.length > 0) {
+      return this.prioritySlides.shift();
+    }
+
+    return currentSlide
+  }
+
   firebaseSlideListener(key) {
     this.props.firebase.slides().orderByKey().startAt(key).on("child_added", snapshot => {
       const slide = snapshot.val();
       const slideKey = snapshot.key
-
+      console.log('child added! yay', slideKey)
       if (key !== slideKey) {
-        this.preloadSlide(slide, key)
+        this.preloadSlide(slide, key, true)
       }
     });
   }
 
-  preloadSlide(slide, key) {
+  preloadSlide(slide, key, isPriority = false) {
     const img = new Image();
 
     img.onload = () => {
@@ -92,6 +102,10 @@ class SlideShow extends Component {
       slide.key = key
 
       this.slides = [slide, ...this.slides];
+      if (isPriority) {
+        console.log('prio slides')
+        this.prioritySlides = [...this.prioritySlides, slide];
+      }
     }
 
     this.props.firebase
