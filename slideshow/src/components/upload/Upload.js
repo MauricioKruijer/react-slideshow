6a49da80452601fa3ebea59e5b26e31b1d0580b2
Gustavo Siqueira <gustavo.anacleto@gmail.com>
2018-12-31 12:43:19 +0100

    changed upload page a bit

--- slideshow/src/components/upload/Upload.js
+++ slideshow/src/components/upload/Upload.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
-import FirebaseContext from "../Firebase/Context";
-import FileUploader from 'react-firebase-file-uploader'
+import FirebaseContext from '../Firebase/Context';
+import FileUploader from 'react-firebase-file-uploader';
 
 import './upload.css';
 
 class Upload extends Component {
   state = {
-    username: "",
-    avatar: "",
+    username: '',
+    avatar: '',
     isUploading: false,
     progress: 0,
-    avatarURL: ""
+    avatarURL: '',
   };
-
-  handleChangeUsername = event => this.setState({ username: event.target.value });
 
   handleUploadStart = () => this.setState({ isUploading: true, progress: 0 });
 
@@ -27,24 +25,26 @@ class Upload extends Component {
   handleUploadSuccess = filename => {
     this.setState({ avatar: filename, progress: 100, isUploading: false });
 
-    this.props.firebase
-      .storage
+    this.props.firebase.storage
       .ref('files')
       .child(filename)
       .getDownloadURL()
       .then(url => {
-        this.setState({ avatarURL: url })
-        this.saveFile(url)
+        this.setState({ avatarURL: url });
+        this.saveFile(url);
       });
   };
 
   saveFile(url) {
     this.props.firebase.db.ref('files').push({
-      url: url
-    })
+      url: url,
+    });
   }
 
   render() {
+    const { firebase } = this.props;
+    const { avatarURL, isUploading } = this.state;
+
     return (
       <div className="upload">
         <h1>Upload your best image</h1>
@@ -53,26 +53,25 @@ class Upload extends Component {
             accept="video/*,image/*"
             name="avatar"
             randomizeFilename
-            storageRef={this.props.firebase.storage.ref('files')}
+            storageRef={firebase.storage.ref('files')}
             onUploadStart={this.handleUploadStart}
             onUploadError={this.handleUploadError}
             onUploadSuccess={this.handleUploadSuccess}
             onProgress={this.handleProgress}
-            metadata={{cacheControl: 'max-age=300'}}
+            metadata={{ cacheControl: 'max-age=300' }}
           />
 
-          {this.state.avatarURL
-            ? <img alt="" src={this.state.avatarURL} height={300}  />
-            : <p>Drag your files here or click in this area.</p>
-          }
+          {/* {!isUploading ? <button type="submit">Submit</button> : null} */}
 
-          <button type="submit">Upload</button>
+          {avatarURL ? <p>✅</p> : !isUploading ? <span className="rotate">♻️</span> : null}
         </form>
       </div>
     );
   }
 }
 
-export default (props) => <FirebaseContext.Consumer>
-  { firebase => <Upload firebase={firebase} {...props}/> }
-</FirebaseContext.Consumer>
+export default props => (
+  <FirebaseContext.Consumer>
+    {firebase => <Upload firebase={firebase} {...props} />}
+  </FirebaseContext.Consumer>
+);
