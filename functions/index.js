7fdd6d0b7aa702d6a3f75020223796abfc551f4e
Mauricio Kruijer <mauricio.kruijer@gmail.com>
2018-12-31 15:08:56 +0100

    Added video cloud function to strip off the audio
    and convert always to mp4

--- functions/index.js
+++ functions/index.js
@@ -10,6 +10,8 @@ const path = require('path');
 const os = require('os');
 const fs = require('fs');
 
+const ffmpeg = require('fluent-ffmpeg');
+
 /**
  * When an image is uploaded in the Storage bucket We generate a thumbnail automatically using
  * ImageMagick.
@@ -100,4 +102,101 @@ exports.ProcessImage = functions.storage.object().onFinalize((object) => {
       meta
     })
   });
+});
+
+exports.ProcessVideo = functions.storage.object().onFinalize((object) => {
+  const filePath = object.name;
+  const name = object.name;
+  const contentType = object.contentType; // This is the image MIME type
+  const fileDir = path.dirname(filePath);
+  const fileName = path.basename(filePath);
+  const thumbFilePath = path.normalize(path.join(fileDir, `final_${fileName}`));
+  const tempLocalFile = path.join(os.tmpdir(), filePath);
+  const tempLocalDir = path.dirname(tempLocalFile);
+  const tempLocalThumbFile = path.join(os.tmpdir(), thumbFilePath);
+
+  // Exit if this is triggered on a file that is not an image.
+  if (!contentType.startsWith('video/')) {
+    console.log('This is not a video.');
+    return null;
+  }
+
+  if (fileName.startsWith('final_')) {
+    return console.log('Already converted.');
+  }
+
+  // Cloud Storage files.
+  const bucket = storage.bucket(object.bucket);
+  const file = bucket.file(filePath);
+  const thumbFile = bucket.file(thumbFilePath);
+  const metadata = {
+    contentType: contentType,
+    // To enable Client-side caching you can set the Cache-Control headers here. Uncomment below.
+    // 'Cache-Control': 'public,max-age=3600',
+  };
+
+  // Create the temp directory where the storage file will be downloaded.
+  return mkdirp(tempLocalDir).then(() => {
+    // Download file from bucket.
+    return file.download({destination: tempLocalFile});
+  }).then(() => {
+    console.log('The file has been downloaded to ADDED NEW LINE', tempLocalFile);
+    // Generate a thumbnail using ImageMagick.
+    console.log('files', {
+      fromFirebase: tempLocalFile,
+      newTarget: tempLocalThumbFile,
+    })
+    return new Promise((resolve, reject) => {
+      ffmpeg(tempLocalFile)
+        .noAudio()
+        .on('start', function (commandLine) {
+          console.log('Spawned Ffmpeg with command: ' + commandLine);
+        })
+        .on('end', function () {
+          console.log('Finished processing', `final_${fileName}.mp4`);
+          resolve(`final_${fileName}.mp4`)
+        })
+        .output(`${tempLocalThumbFile}.mp4`)
+        .run();
+    })
+  }).then(newupload => {
+    console.log("UPLOAD CONVERTED FILE ", newupload, `${tempLocalThumbFile}.mp4`)
+
+    return bucket.upload(`${tempLocalThumbFile}.mp4`, {
+      destination: thumbFilePath + '.mp4',
+      metadata: metadata,
+    });
+  }).then((capture) => {
+    console.log("=== heb  je iets PROBEWEEE ===")
+    return new Promise((resolve, reject) => {
+      ffmpeg.ffprobe(tempLocalFile, function(err, metadata) {
+        if (err) {
+          console.log('error in probe')
+          console.error('error in probe')
+          reject('error')
+          return;
+        }
+
+        metadata.streams.forEach(stream => {
+          if (stream.codec_type === 'video') {
+            return resolve({
+              width: stream.width,
+              height: stream.height,
+              duration: Math.ceil(stream.duration) * 1000
+            })
+          }
+        })
+      });
+    });
+    // console.log('Thumbnail created at', finalLocalThumbFile);
+    // Uploading the Thumbnail.
+    // return bucket.upload(finalLocalThumbFile, {destination: thumbFilePath, metadata: metadata});
+  }).then((meta) => {
+    console.log('==!! UPLOAD TO ', `final_${fileName}.mp4`)
+    return admin.database().ref('video').push({
+      name:`files/final_${fileName}.mp4`,
+      type: 'video',
+      meta
+    })
+  })
 });
